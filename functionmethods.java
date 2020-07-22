a364bbbe10911004560952ee27783b5b5f872815
Z Kaoher <kaohzina@gmail.com>
2020-07-22 22:09:42 +0000

    Made a return function to indicate quantity from array

--- functionmethods.java
+++ functionmethods.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class functionmethods{
  static void product(String name, int price) {
     System.out.println(name + " costs " + "$"+price);
-    System.out.println(quantity[1]);
+      int[]quantity = available(5,10,15,20);
+      System.out.println(quantity[1]);
+
     }
 
   public static void main(String[] args){
@@ -11,9 +13,9 @@ public class functionmethods{
   product("Pohtahto",5000);
   product("Tohmahto",5000);
   }
-  int[]quantity = available(5,10,15,20);
+
   
-  public static int[] available(int a, int b, int c, int d); {
+  public static int[] available(int a, int b, int c, int d) {
   int[] instock = new int [4];
   instock[0] = a;
   instock[1] = b;
