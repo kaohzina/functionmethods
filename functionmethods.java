19d277713cf412736cdb7727e175366ed454cc00
Z Kaoher <kaohzina@gmail.com>
2020-07-22 19:08:35 +0000

    Added product method

--- functionmethods.java
+++ functionmethods.java
@@ -1,10 +1,13 @@
 import java.util.*;
-public class {
-  static void method() {
-  
-  }
-  public static void main(Strings[] args){
-    Scanner user_input = new Scanner(System.in);
+public class functionmethods{
+ static void product(String name, int price) {
+    System.out.println(name + " costs " + "$"+price);
+    }
 
+  public static void main(String[] args){
+  product("Potatoe",400);
+  product("Tomatoe",400);
+  product("Pohtahto",5000);
+  product("Tohmahto",5000);
   }
 }
